56e43b92590a9ffeacec0e1dd05e500f0c746762
Yae Young Jun <yaeyoungjun@gmail.com>
2019-04-14 20:28:23 +1000

    adds functions for getting, adding and deleting superpowers

--- src/app/superpowers/superpowers.component.ts
+++ src/app/superpowers/superpowers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Superpower } from '../superpower';
+import { SuperpowerService } from '../superpower.service';
+
 @Component({
   selector: 'app-superpowers',
   templateUrl: './superpowers.component.html',
@@ -9,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SuperpowersComponent implements OnInit {
+  superpowers: Superpower[];
 
-  constructor() { }
+  constructor(private superpowerService: SuperpowerService) { }
 
   ngOnInit() {
+    this.getSuperpowers();
+  }
+
+  getSuperpowers(): void {
+    this.superpowerService.getSuperpowers()
+    .subscribe(superpowers => this.superpowers = superpowers);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.superpowerService.addSuperpower({ name } as Superpower)
+      .subscribe(superpower => {
+        this.superpowers.push(superpower);
+      });
+  }
+
+  delete(superpower: Superpower): void {
+    this.superpowers = this.superpowers.filter(h => h !== superpower);
+    this.superpowerService.deleteSuperpower(superpower).subscribe();
   }
 
 }
