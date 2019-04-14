72b60ea757dfe2b85fa15f19d52c0b6f62a75fc7
Yae Young Jun <yaeyoungjun@gmail.com>
2019-04-14 17:47:21 +1000

    adds superpowers link and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
+import { SuperpowersComponent }      from './superpowers/superpowers.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'superpowers', component: SuperpowersComponent }
 ];
 
 @NgModule({
