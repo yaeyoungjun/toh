6a7f1f408e515cd527f67403e084bf5582d76053
Yae Young Jun <yaeyoungjun@gmail.com>
2019-04-15 23:36:15 +1000

    link superpower to hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Hero }         from '../hero';
+import { Superpower } from '../superpower';
 import { HeroService }  from '../hero.service';
+import { SuperpowerService } from '../superpower.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -12,15 +14,19 @@ import { HeroService }  from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+  superpowers: Superpower[];
+  heroSuperpower: number;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
+    private superpowerService: SuperpowerService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.getHero();
+    this.getSuperpowers();
   }
 
   getHero(): void {
@@ -29,12 +35,24 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  getSuperpowers(): void {
+    this.superpowerService.getSuperpowers()
+    .subscribe(superpowers => this.superpowers = superpowers);
+  }
+
   goBack(): void {
     this.location.back();
   }
 
  save(): void {
+    if (this.heroSuperpower) {
+       if (!this.hero.superpowers) {
+         this.hero.superpowers = [];
+       }
+      const selectedPower = this.superpowers.find(power => power.id == this.heroSuperpower)
+      this.hero.superpowers.push(selectedPower);
+    }
     this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+      .subscribe();
   }
 }
